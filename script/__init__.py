cc92966cda3aa9deb9a223858d420b6198f16b4b
TomokiHirose <hirose.tomoki@gmail.com>
2020-11-14 22:02:54 +0900

    フォーマットとリント用のスクリプト追加と環境アップデート

---
+++ script/__init__.py
@@ -0,0 +1,32 @@
+import toml
+import logzero
+from logzero import logger
+from subprocess import check_call, call
+
+formatter = logzero.LogFormatter(
+    fmt="%(color)s[%(levelname)1.1s %(asctime)s]%(end_color)s %(message)s"
+)
+logzero.setup_default_logger(formatter=formatter)
+
+
+with open("pyproject.toml") as f:
+    conf = toml.load(f)
+PACKAGE_NAME = conf["tool"]["poetry"]["name"]
+
+
+def format():
+    logger.info("Running isort...")
+    call(["isort", PACKAGE_NAME])
+    logger.info("Running black...")
+    call(["black", PACKAGE_NAME])
+
+
+def lint():
+    logger.info("Running mypy...")
+    call(["mypy", PACKAGE_NAME])
+    logger.info("Running flake8...")
+    call(["flake8", "template"])
+
+
+def document():
+    print("document")
